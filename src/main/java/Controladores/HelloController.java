3bb1d7264f671a69fd69cb65947dc92b3d09f746
Carlos Rojas <carlosrojasmartinez19@gmail.com>
2024-09-24 00:03:19 -0500

    Se cargo visto de Registro

--- src/main/java/Controladores/HelloController.java
+++ src/main/java/Controladores/HelloController.java
@@ -1,14 +1,34 @@
 package Controladores;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public MenuItem optSalir;
+    @FXML
+    public BorderPane contenedor;
+
+    public HelloController() throws IOException {
     }
+
+    @FXML
+    public void salir() {
+        System.exit(0);
+    }
+
+    @FXML
+    public void iniciarSesion() throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("Vistas/View-Login.fxml"));
+        contenedor.setCenter(pane);
+    }
+
+
 }
