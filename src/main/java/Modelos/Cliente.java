51826ad2c1ce27b76a92ede324e424d6d1dc3179
Juan Martín Sánchez <jmartin_sanchez@javeriana.edu.co>
2024-10-06 17:26:47 -0500

    Adicion de algunos metodos en las clases

--- src/main/java/Modelos/Cliente.java
+++ src/main/java/Modelos/Cliente.java
@@ -7,6 +7,7 @@ public class Cliente {
     private String correoElectronico;
     private String telefono;
     private int idTienda;
+
     public int getIdCliente() {
         return idCliente;
     }
