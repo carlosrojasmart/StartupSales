504a6a520918642429addb3277fa00d8a2b94084
Mariana Perez Casallas <95313566+mperez1516@users.noreply.github.com>
2024-11-04 21:43:10 -0500

    Se elimino el archivo de dependencia de sql

---
+++ src/test/java/Servicios/Datos/CrearProductoTest.java
@@ -0,0 +1,36 @@
+package Servicios.Datos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CrearProductoTest {
+
+    @Test
+    public void crearProducto() {
+    }
+
+    @Test
+    public void generarIdProductoAleatorio() {
+    }
+
+    @Test
+    public void obtenerProductosDeTienda() {
+    }
+
+    @Test
+    public void actualizarProducto() {
+    }
+
+    @Test
+    public void eliminarProducto() {
+    }
+
+    @Test
+    public void editarProducto() {
+    }
+
+    @Test
+    public void formatearPrecio() {
+    }
+}
---
+++ src/test/java/Servicios/Datos/CrearTiendaTest.java
@@ -0,0 +1,28 @@
+package Servicios.Datos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CrearTiendaTest {
+
+    @Test
+    public void cargarImagen() {
+    }
+
+    @Test
+    public void crearTienda() {
+    }
+
+    @Test
+    public void generarIdAleatorio() {
+    }
+
+    @Test
+    public void eliminarTienda() {
+    }
+
+    @Test
+    public void getArchivoImagen() {
+    }
+}
